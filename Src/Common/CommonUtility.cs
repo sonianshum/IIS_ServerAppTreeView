cc8d0416d1a2b87b3c553baff557d7690c2f1aa3
Anshum Soni <anshumsoni@yahoo.com>
2018-12-10 01:31:23 +0530

    clean up files

--- Src/Common/CommonUtility.cs
+++ Src/Common/CommonUtility.cs
@@ -1,31 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-/*
-* ONE IDENTITY LLC.PROPRIETARY INFORMATION
-*
-* This software is confidential.One Identity, LLC.or one of its affiliates or
-* subsidiaries, has supplied this software to you under terms of a
-* license agreement, nondisclosure agreement or both.
-*
-* You may not copy, disclose, or use this software except in accordance with
-*  those terms.
-*
-*
-* Copyright 2017 One Identity LLC.
-* ALL RIGHTS RESERVED.
-*
-* ONE IDENTITY LLC.MAKES NO REPRESENTATIONS OR
-* WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
-* TO THE IMPLIED WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE, OR
-* NON-INFRINGEMENT.ONE IDENTITY LLC.SHALL NOT BE
-* LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE
-* AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
-* THIS SOFTWARE OR ITS DERIVATIVES.
-*/
-// --------------------------------------------------------------------------------------------------------------------
-
-namespace Starling.HttpModule
+﻿namespace ServerTreeView.Common
 {
     #region using statements
     using System;
@@ -36,9 +9,9 @@ namespace Starling.HttpModule
     #endregion
     public static class CommonUtility
     {
-        private const string RegistryKeyPath = @"SOFTWARE\One Identity\Starling Two-Factor HTTP Module";
+        private const string RegistryKeyPath = @"SOFTWARE\Module";
         private const string DiagnosticKey = @"Diagnostics";
-        internal const string EventSource = @"Starling Two-Factor HTTP Module";
+        internal const string EventSource = @"Module";
         /// <summary>
         /// Gets HttpModule installation path from registry
         /// </summary>
@@ -143,7 +116,6 @@ namespace Starling.HttpModule
         /// <param name="errormessage">error message</param>
         /// <param name="logEntryType">type of an event log entry</param>
         /// <param name="eventIdType"></param>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
         private static void WriteIntoEventLog(string errormessage, EventLogEntryType logEntryType, int eventIdType)
         {
             // Configure event logging
